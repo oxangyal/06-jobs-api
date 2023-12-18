9ed0728c29df9c18463680ab68e262b85fdb2443
oxangyal <Oxana.mitch@gmail.com>
2023-12-18 13:27:52 -0500

    CRUD operations for the API, error handler

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//extra security packages
+const helmet = require('helmet');
+const cors = require('cors')
+const xss = require('xss-clean');
+const rateLimiter = require("express-rate-limit");
+
+
+// //Swagger
+// const swaggerUI = require("swagger-ui-express");
+// const YAML = require("yamljs");
+// const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -13,9 +26,18 @@ const workoutsRouter = require("./routes/workouts");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(express.json());
-// extra packages
+app.set("trust proxy", 1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, //15 min
+        max: 100, //limit each IP to 100 requests per windowMs
+    })
+);
 
+app.use(express.json());
+app.use(helmet())
+app.use(cors());
+app.use(xss());
 //routes
 
 app.use("/api/v1/auth", authRouter);
--- controllers/workout.js
+++ controllers/workout.js
@@ -1,35 +1,86 @@
 const Workout = require("../models/Workout");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
+const { params } = require("../routes/auth");
 
 const getAllWorkouts = async (req, res) => {
-    res.send("get all workouts");
-};
+    // console.log("KUKU: req.user: " + JSON.stringify(req.user));
 
+    const workouts = await Workout.find({ createdBy: req.user.userId }).sort(
+        "createdAt"
+    );
+
+    // console.log("KUKU: workouts: " + JSON.stringify(workouts));
+
+    res.status(StatusCodes.OK).json({ workouts, count: workouts.length });
+};
 const getWorkout = async (req, res) => {
-    res.send("get single workout");
+    const {
+        user: { userId },
+        params: { id: workoutId },
+    } = req;
+
+    // console.log("KUKU: user: " + user + ", params: " + JSON.stringify(params));
+
+    const workout = await Workout.findOne({
+        _id: workoutId,
+        createdBy: userId,
+    });
+    if (!workout) {
+        throw new NotFoundError(`No workout with id ${workoutId}`);
+    }
+    res.status(StatusCodes.OK).json({ workout });
 };
 
 const createWorkout = async (req, res) => {
-    //    console.log(req.user);
     req.body.createdBy = req.user.userId;
-
     const workout = await Workout.create(req.body);
     res.status(StatusCodes.CREATED).json({ workout });
 };
 
 const updateWorkout = async (req, res) => {
-    res.send("update workout");
+    const {
+        body: { workoutType, duration },
+        user: { userId },
+        params: { id: workoutId },
+    } = req;
+
+    if (workoutType === "" || duration === "") {
+        throw new BadRequestError(
+            "Workout type or Duration fields cannot be empty"
+        );
+    }
+    const workout = await Workout.findByIdAndUpdate(
+        { _id: workoutId, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    );
+    if (!workout) {
+        throw new NotFoundError(`No workout with id ${workoutId}`);
+    }
+    res.status(StatusCodes.OK).json({ workout });
 };
 
 const deleteWorkout = async (req, res) => {
-    res.send("delete workout");
+    const {
+        user: { userId },
+        params: { id: workoutId },
+    } = req;
+
+    const workout = await Workout.findByIdAndRemove({
+        _id: workoutId,
+        createdBy: userId,
+    });
+    if (!workout) {
+        throw new NotFoundError(`No workout with id ${workoutId}`);
+    }
+    res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
-    getAllWorkouts,
-    getWorkout,
     createWorkout,
-    updateWorkout,
     deleteWorkout,
+    getAllWorkouts,
+    updateWorkout,
+    getWorkout,
 };
